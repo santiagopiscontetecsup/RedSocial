dc747d227aae62fe29ab3850fa5d47ca84bcef96
He4rtttt <kevin.caya@tecsup.edu.pe>
2025-05-21 10:20:45 -0500

    Cambios antes de prebuild

--- app.config.js
+++ app.config.js
@@ -1,7 +1,9 @@
+import 'dotenv/config'; // 👈 IMPORTANTE para cargar las variables del archivo .env
+
 export default {
   expo: {
-    name: 'RedSocial',
-    slug: 'RedSocial',
+    name: 'Full Chamba',
+    slug: 'Full Chamba',
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
@@ -12,23 +14,43 @@ export default {
       resizeMode: 'contain',
       backgroundColor: '#ffffff'
     },
-    assetBundlePatterns: [
-      '**/*'
-    ],
+    assetBundlePatterns: ['**/*'],
     ios: {
       supportsTablet: true
     },
     android: {
+      package: 'com.fullChamba.redsocial',
+      versionCode: 1,
       adaptiveIcon: {
-        foregroundImage: './assets/images/adaptive-icon.png',
+        foregroundImage: './assets/images/adaptive-icon.png', // Usa la imagen cuadrada
         backgroundColor: '#ffffff'
       }
     },
     web: {
-      favicon: './assets/images/favicon.png'
+      favicon: './assets/images/favicon.png',
+      bundler: 'metro',
+      output: 'static'
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-splash-screen',
+        {
+          image: './assets/images/splash-icon.png',
+          imageWidth: 200,
+          resizeMode: 'contain',
+          backgroundColor: '#ffffff'
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: false
     },
     extra: {
-      API_BASE_URL: process.env.API_BASE_URL
+      API_BASE_URL: process.env.API_BASE_URL,
+      eas: {
+        projectId: 'd916c8be-fc6f-4594-93fd-b25ab6bd928f'
+      }
     }
   }
 }
